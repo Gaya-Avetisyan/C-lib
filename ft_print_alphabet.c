50a37562c0478576099293102558ebf3111e4147
Gayane Avetisyan <gaya.avetisian@gmaail.com>
2022-04-06 16:57:58 +0400

    4 functions

---
+++ ft_print_alphabet.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void);
+
+int main(void)
+{
+	ft_print_alphabet();
+return(0);
+}
+
+void ft_print_alphabet(void)
+{
+char a;
+a = 'a';
+while(a < 'z')
+{
+	write(1, &a, 1);
+	
+
+a++;
+}
+
+	write(1,"\n",1);
+}
---
+++ ft_print_numbers.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_print_numbers(void)
+{
+	char i = '0';
+	while(i<='9')
+	{
+		write(1, &i, 1);
+	i++;
+	}
+}
+
+int main(void)
+{
+	ft_print_numbers();
+	write(1, "\n",1);
+	return(0);
+}
